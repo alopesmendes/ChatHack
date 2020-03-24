0cfb21a4c7deefe7d2bf20d40766424768ec5092
alopesme <alopesme@etud.u-pem.fr>
2020-03-24 20:38:54 +0100

    Test

--- ChatHack/src/fr/upem/net/tcp/frame/test/DataTest.java
+++ ChatHack/src/fr/upem/net/tcp/frame/test/DataTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 
 import fr.upem.net.tcp.frame.Data;
@@ -13,55 +15,146 @@ class DataTest {
 	@Test
 	void TestCreationParametersShouldNotBeNull() {
 		assertAll(
-			() -> assertThrows(NullPointerException.class, () -> Data.createDataText(null)),
-			() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, null, null)),
-			() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, null, "")),
-			() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, "", null))
-		);
+				() -> assertThrows(NullPointerException.class, () -> Data.createDataText(null)),
+				() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, null, null)),
+				() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, null, "")),
+				() -> assertThrows(NullPointerException.class, () -> Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)0, "", null))
+				);
 	}
-	
+
 	private void TestSameDataText() {
 		Data d1 = Data.createDataText("bonjour");
 		Data d2 = Data.createDataText("bonjour");
 		Data d3 = Data.createDataText("bonjour");
 		assertAll(
-			() -> assertEquals(d1, d1),
-			() -> assertEquals(d1, d2),
-			() -> assertEquals(d2, d3),
-			() -> assertEquals(d1, d3)
-		);
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
 	}
-	
+
 	private void TestSameDataGlobal() {
 		Data d1 = Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Will", "bonjour");
 		Data d2 = Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Will", "bonjour");
 		Data d3 = Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Will", "bonjour");
 		assertAll(
-			() -> assertEquals(d1, d1),
-			() -> assertEquals(d1, d2),
-			() -> assertEquals(d2, d3),
-			() -> assertEquals(d1, d3)
-		);
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
 	}
-	
+
 	private void TestSameDataError() {
 		Data d1 = Data.createDataError(StandardOperation.ERROR, (byte)1);
 		Data d2 = Data.createDataError(StandardOperation.ERROR, (byte)1);
 		Data d3 = Data.createDataError(StandardOperation.ERROR, (byte)1);
 		assertAll(
-			() -> assertEquals(d1, d1),
-			() -> assertEquals(d1, d2),
-			() -> assertEquals(d2, d3),
-			() -> assertEquals(d1, d3)
-		);
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+
+	private void TestSameDataGlobalClient() {
+		Data d1 = Data.createDataGlobalClient(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Ici");
+		Data d2 = Data.createDataGlobalClient(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Ici");
+		Data d3 = Data.createDataGlobalClient(StandardOperation.GLOBAL_MESSAGE, (byte)2, "Ici");
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+
+	private void TestSameDataConnectionServerMdp() {
+		Data d1 = Data.createDataConnectionServerMdp(Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Will", Optional.of("lol")));
+		Data d2 = Data.createDataConnectionServerMdp(Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Will", Optional.of("lol")));
+		Data d3 = Data.createDataConnectionServerMdp(Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Will", Optional.of("lol")));
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+
+	private void TestSameDataConnectionClient() {
+		Data d1 = Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Bob", Optional.of("man"));
+		Data d2 = Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Bob", Optional.of("man"));
+		Data d3 = Data.createDataConnectionClient(StandardOperation.CONNEXION, (byte)1, "Bob", Optional.of("man"));
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
 	}
 	
+	private void TestSameDataConnectionServerMdpReponse() {
+		Data d1 = Data.createDataConnectionServerMdpReponse((byte)0, 1);
+		Data d2 = Data.createDataConnectionServerMdpReponse((byte)0, 1);
+		Data d3 = Data.createDataConnectionServerMdpReponse((byte)0, 1);
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+	
+	private void TestSameDataPrivateConnectionAccepted() {
+		Data d1 = Data.createDataPrivateConnectionAccepted(StandardOperation.PRIVATE_CONNEXION, (byte)5, "Will", 7777, "localhost", 1);
+		Data d2 = Data.createDataPrivateConnectionAccepted(StandardOperation.PRIVATE_CONNEXION, (byte)5, "Will", 7777, "localhost", 1);
+		Data d3 = Data.createDataPrivateConnectionAccepted(StandardOperation.PRIVATE_CONNEXION, (byte)5, "Will", 7777, "localhost", 1);
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+	
+	private void TestSameDataPrivateConnectionReponse() {
+		Data d1 = Data.createDataPrivateConnectionReponse(StandardOperation.PRIVATE_CONNEXION, (byte)4, "Bob", (byte)0);
+		Data d2 = Data.createDataPrivateConnectionReponse(StandardOperation.PRIVATE_CONNEXION, (byte)4, "Bob", (byte)0);
+		Data d3 = Data.createDataPrivateConnectionReponse(StandardOperation.PRIVATE_CONNEXION, (byte)4, "Bob", (byte)0);
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+	
+	private void TestSameDataPrivateConnectionRequested() {
+		Data d1 = Data.createDataPrivateConnectionRequested(StandardOperation.PRIVATE_CONNEXION, (byte)1, "alice");
+		Data d2 = Data.createDataPrivateConnectionRequested(StandardOperation.PRIVATE_CONNEXION, (byte)1, "alice");
+		Data d3 = Data.createDataPrivateConnectionRequested(StandardOperation.PRIVATE_CONNEXION, (byte)1, "alice");
+		assertAll(
+				() -> assertEquals(d1, d1),
+				() -> assertEquals(d1, d2),
+				() -> assertEquals(d2, d3),
+				() -> assertEquals(d1, d3)
+				);
+	}
+
 	@Test
 	void TestSameData() {
 		assertAll(
-			() -> TestSameDataText(),
-			() -> TestSameDataGlobal(),
-			() -> TestSameDataError()
-		);
+				() -> TestSameDataText(),
+				() -> TestSameDataGlobal(),
+				() -> TestSameDataError(),
+				() -> TestSameDataGlobalClient(),
+				() -> TestSameDataConnectionServerMdp(),
+				() -> TestSameDataConnectionClient(),
+				() -> TestSameDataConnectionServerMdpReponse(),
+				() -> TestSameDataPrivateConnectionAccepted(),
+				() -> TestSameDataPrivateConnectionReponse(),
+				() -> TestSameDataPrivateConnectionRequested()
+				);
 	}
 }
--- ChatHack/src/fr/upem/net/tcp/frame/test/FrameTest.java
+++ ChatHack/src/fr/upem/net/tcp/frame/test/FrameTest.java
@@ -1,7 +1,116 @@
 package fr.upem.net.tcp.frame.test;
 
-public class FrameTest {
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import fr.upem.net.tcp.frame.Data;
+import fr.upem.net.tcp.frame.Frame;
+import fr.upem.net.tcp.frame.StandardOperation;
+
+class FrameTest {
+
+	@Test
+	void TestFrameGlobalClient() {
+		byte opcode = StandardOperation.GLOBAL_MESSAGE.opcode();
+		byte step = (byte)1;
+		String message = "Test Global";
+		ByteBuffer bb = Frame.createFrameGlobal(Data.createDataGlobalClient(StandardOperation.GLOBAL_MESSAGE, step, message)).buffer();
+		int fLimit = bb.limit();
+		var op = bb.get();
+		var st= bb.get();
+		int size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String m = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		assertAll(
+				() -> assertEquals(opcode, op),
+				() -> assertEquals(step, st),
+				() -> assertEquals(message, m)
+				);
+	}
+
+	@Test
+	void TestFrameGlobalServer() {
+		byte opcode = StandardOperation.GLOBAL_MESSAGE.opcode();
+		byte step = (byte)1;
+		String login = "Alice";
+		String message = "Test Global";
+		ByteBuffer bb = Frame.createFrameGlobal(Data.createDataGlobalServer(StandardOperation.GLOBAL_MESSAGE, step, login, message)).buffer();
+		int fLimit = bb.limit();
+		var op = bb.get();
+		var st= bb.get();
+		int size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String l = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String m = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		assertAll(
+				() -> assertEquals(opcode, op),
+				() -> assertEquals(step, st),
+				() -> assertEquals(login, l),
+				() -> assertEquals(message, m)
+				);
+	}
 	
+	@Test
+	void TestFrameConnect() {
+		byte opcode = StandardOperation.CONNEXION.opcode();
+		byte connexion = (byte)2;
+		String login = "Alice";
+		Optional<String> password = Optional.of("bob");
+		ByteBuffer bb = Frame.createFrameConnection(Data.createDataConnectionClient(StandardOperation.CONNEXION, connexion, login, password)).buffer();
+		int fLimit = bb.limit();
+		byte op = bb.get();
+		byte co = bb.get();
+		int size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String l = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String p = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		assertAll(
+				() -> assertEquals(opcode, op),
+				() -> assertEquals(connexion, co),
+				() -> assertEquals(login, l),
+				() -> assertEquals(password.get(), p)
+				);
+	}
 	
+	@Test
+	void TestFrameConnectionClient() {
+		byte connexion = (byte)1;
+		String login = "Alice";
+		String password= "Bob";
+		ByteBuffer bb = Frame.createFrameConnectionMdp(
+		Data.createDataConnectionServerMdp(
+		Data.createDataConnectionClient(StandardOperation.CONNEXION, connexion, login, Optional.of(password)))).buffer();
+		int fLimit = bb.limit();
+		byte co = bb.get();
+		bb.getLong();
+		int size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String l = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		size = bb.getInt();
+		bb.limit(bb.position()+size);
+		String p = StandardCharsets.UTF_8.decode(bb).toString();
+		bb.limit(fLimit);
+		assertAll(
+				() -> assertEquals((byte)2, co),
+				() -> assertEquals(login, l),
+				() -> assertEquals(password, p)
+				);
+	}
 
 }
